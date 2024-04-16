dcb93b3cd17ab17e6eee6c2cf324e03a339dd73e
Toon <toon.claessens@vdabcampus.be>
2024-04-16 12:14:20 +0200

    Add project files.

---
+++ CsharpPFCursus/Program.cs
@@ -0,0 +1,21 @@
+﻿//een voorbeeld
+
+Console.WriteLine("Hallo"); // ik zei hallo
+/*ik heb
+hallo gezegd*/
+
+Console.WriteLine(
+ "Hallo");
+
+Console.WriteLine("hal" +
+    "lo");
+
+Console.WriteLine("Hallo");
+
+Tekenlijn();
+
+void Tekenlijn()
+{
+    throw new NotImplementedException();
+}
+
