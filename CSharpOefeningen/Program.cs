5b11b00ec2da6a3b9c3fd9072ae0b71b50c39021
Toon <toon.claessens@vdabcampus.be>
2024-04-16 15:52:44 +0200

    16/04/24 1552

--- CSharpOefeningen/Program.cs
+++ CSharpOefeningen/Program.cs
@@ -1,4 +1,1 @@
-﻿const float GemLichTempCelsius = 37.0F;
-float gemLichTempFahrenheit = GemLichTempCelsius * 9.0F / 5.0F + 32.0F;
-Console.WriteLine("Gem. lichaamstemperatuur in graden Celsius: " + GemLichTempCelsius);
-Console.WriteLine("Gem. lichaamstemperatuur in graden Fahrenheit: " + gemLichTempFahrenheit);
+﻿
